fc80744eba1e9cb0eb216d4a3bd930319af9e271
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-24 16:55:41 +0530

    Added:Employee attendance

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,7 +1,13 @@
 public class EmpWage {
 
     public static void main(String[] args) {
-      System.out.println("Welcome to EmpWage");
+int IS_FULL_TIME = 0;
+        double empCheck= Math.floor(Math.random()*10) % 2 ;
 
+      System.out.println(empCheck);
+      if ( empCheck == IS_FULL_TIME )
+        System.out.println("Employee is present");
+        else
+        System.out.println("Employee is absent");
     }
 }
